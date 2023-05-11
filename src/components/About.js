60aec34dea11a166a43fdda3454a34ab29d53b88
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-05-11 16:55:38 +0200

    add About

--- src/components/About.js
+++ src/components/About.js
@@ -1,33 +1,7 @@
-/* Styles */
-import "../styles/App.scss";
-/* HOOKS */
-import { Routes, Route } from "react-router-dom";
 import React from "react";
-/* SECCIÓN DE IMPORT */
-import About from "./About";
-import Home from "./Home";
-import Projects from "./Projects";
-import Contact from "./Contact";
-import Navbar from "./Navbar";
 
-/* ROUTES */
-function App() {
-  return (
-    <div>
-      <div>
-        <Navbar></Navbar>
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/Projects" element={<Projects />} />
-            <Route path="/Contact" element={<Contact />} />
-            {/* <Route path="*" element={<Error404 />} /> */}
-          </Routes>
-        </main>
-      </div>
-    </div>
-  );
-}
+const About = () => {
+  return <div></div>;
+};
 
-export default App;
+export default About;
