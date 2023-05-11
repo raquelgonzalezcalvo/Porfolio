1aa6d1171f57c9981baa4696568a466814cf4910
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-05-11 16:46:16 +0200

    add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,34 @@
-import '../styles/App.scss';
+/* Styles */
+import "../styles/App.scss";
+
+/* HOOKS */
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+// import Header from "./Header";
+import About from "./About";
+import Home from "./Home";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import Navbar from "./Navbar";
+/* SECCIÓN DE IMPORT */
 
 function App() {
-  return <div className="App">{/* Aquí va el HTML */}</div>;
+  return (
+    <div>
+      <div>
+        <Navbar></Navbar>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Projects" element={<Projects />} />
+            <Route path="/Contact" element={<Contact />} />
+            {/* <Route path="*" element={<Error404 />} /> */}
+          </Routes>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
