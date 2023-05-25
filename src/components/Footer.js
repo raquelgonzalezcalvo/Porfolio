87f9f463cc0834a62070e87aa3f90776bbc28f03
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-05-25 16:55:18 +0200

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/components/Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="final__text">
+      <p>
+        Copyright &copy; All rights reserved
+        <br />
+        Designed by Raquel González, 2023
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
